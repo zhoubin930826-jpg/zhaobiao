f44dfc908df750c3c7f07c539c7d13096f6ba53b
zhoubin14524 <zhoubin14524@163.com>
2026-05-07 00:45:04 +0800

    调整门户网站处理

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,20 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS
+  sys_role_menu,
+  sys_role_permission,
+  sys_user_role,
+  portal_member_business_type_rel,
+  biz_tender_attachment,
+  sys_user_audit_record,
+  biz_tender,
+  biz_file_storage,
+  portal_member_user,
+  biz_business_type,
+  sys_operation_log,
+  sys_menu,
+  sys_permission,
+  sys_role,
+  sys_user;
+
+SET FOREIGN_KEY_CHECKS = 1;
